07afdaae6524a7bed4288914b2f6815b3688c2a4
ash214-an <intimacy222@naver.com>
2025-08-07 12:58:59 +0900

    add  AxeTrap class

---
+++ Source/Team01/Item/AxeTrap.cpp
@@ -0,0 +1,69 @@
+#include "AxeTrap.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/Character.h"
+#include "Kismet/GameplayStatics.h"
+#include "TimerManager.h"
+
+AAxeTrap::AAxeTrap()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	AxeMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("AxeMesh"));
+	AxeMesh->SetupAttachment(RootComponent);
+
+	DamageBox = CreateDefaultSubobject<UBoxComponent>(TEXT("DamageBox"));
+	DamageBox->SetupAttachment(AxeMesh);
+	DamageBox->SetCollisionProfileName(TEXT("Trigger"));
+	DamageBox->SetGenerateOverlapEvents(true);
+
+	DamageBox->OnComponentBeginOverlap.AddDynamic(this, &AAxeTrap::OnOverlapBegin);
+
+	bCanDamage = true;
+	RunningTime = 0.f;
+}
+
+void AAxeTrap::BeginPlay()
+{
+	Super::BeginPlay();
+	InitialRotation = AxeMesh->GetRelativeRotation();
+	
+}
+
+void AAxeTrap::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	RunningTime += DeltaTime;
+	float SwingAngle = FMath::Sin(RunningTime * 2.f) * 45.f;
+	FRotator NewRotation = InitialRotation + FRotator(0.f, SwingAngle, 0.f); // Yaw 기준 회전
+	AxeMesh->SetRelativeRotation(NewRotation);
+
+}
+
+void AAxeTrap::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+	bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!bCanDamage || !IsValid(OtherActor))
+	{
+		return;
+	}
+	
+	ACharacter* Player = Cast<ACharacter>(OtherActor);
+	if (Player)
+	{
+		UGameplayStatics::ApplyDamage(Player, Damage, GetInstigatorController(), this, nullptr);
+
+		bCanDamage = false;
+		GetWorld()->GetTimerManager().SetTimer(InvincibilityTimer, this, &AAxeTrap::ResetDamage, InvincibilityDuration, false);
+	}
+}
+
+void AAxeTrap::ResetDamage()
+{
+	bCanDamage = true;
+}
+
---
+++ Source/Team01/Item/AxeTrap.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AxeTrap.generated.h"
+
+UCLASS()
+class TEAM01_API AAxeTrap : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AAxeTrap();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, Category = "Trap")
+	UStaticMeshComponent* AxeMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Trap")
+	class UBoxComponent* DamageBox;
+
+	float RunningTime = 0.f;
+	FRotator InitialRotation;
+
+	UPROPERTY(EditAnywhere, Category = "Damage")
+	float Damage = 10.f;
+
+
+	UPROPERTY(EditAnywhere, Category = "Damage")
+	float InvincibilityDuration = 1.0f;
+
+	bool bCanDamage = true;
+	FTimerHandle InvincibilityTimer;
+
+	void ResetDamage();
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+		bool bFromSweep, const FHitResult& SweepResult);
+};
